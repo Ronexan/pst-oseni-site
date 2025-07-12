a46e907fa87ec92e3eeb41058409fa3fe2090d58
David Eden <daveyedenex@gmail.com>
2025-07-12 22:39:07 +0100

    Fixed dumb DevTools bug

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -11,7 +11,7 @@ export const Route = createRootRoute({
       <Header />
       <Outlet />
       <Footer />
-      {/* <TanStackRouterDevtools /> */}
+      <TanStackRouterDevtools />
     </>
   ),
   notFoundComponent: NotFound,
